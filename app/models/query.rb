cae2ba1cb2f64b1f6a29f6143878f5ac4d928bcf
Andrew Freeberg <afreeberg@afreeberg.(none)>
2009-12-30 18:04:40 -0500

    More modifications to query views.

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,5 +2,10 @@ class Query < ActiveRecord::Base
   set_primary_key :query_id
   attr_accessible :query_id, :startdate, :enddate, :closed, :lastrun
   has_and_belongs_to_many :sf_users
+
+  def owner_names
+    userarray = sf_users.collect { |sfu| sfu.name }
+    userarray.compact.join(', ')
+  end
   
 end
